ef4b548b2fe3baaf3ac1d4e834c45490b8faa274
Pulkit <pulkitisme@gmail.com>
2014-12-31 21:39:39 +0530

    Created Main Page

--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from bookmarks.views import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_bookmarks.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'django_bookmarks.views.home', name='home'),
+                       url(r'^$', main_page),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+
+
+                       )
