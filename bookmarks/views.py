62353675ed3feb4f14c25b923786582573c64feb
Pulkit <pulkitisme@gmail.com>
2015-01-01 01:03:15 +0530

    Created User Registration Form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
-# from django.template import RequestContext
+from django.contrib.auth import logout
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from bookmarks.forms import *
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username = form.clean_data('username'),
+                password = form.clean_data('password1'),
+                email = form.clean_data('email')
+            )
+            return HttpResponseRedirect('/')
+        else:
+            form = RegistrationForm()
+        variables = RequestContext(request, {
+            'form': form
+        })
+        return render_to_response(
+            'registration/register.html',
+        )
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-        'head_title': 'Django Bookmarks',
-        'page_title': 'Welcome to Django Bookmarks',
-        'page_body': 'Where you can store and share bookmarks!'
-    })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'main_page.html', RequestContext(request)
+    )
 
 
 def user_page(request, username):
@@ -25,11 +47,8 @@ def user_page(request, username):
     except:
         raise Http404('Requested user not found.')
     bookmarks = user.bookmark_set.all()
-
-    template = get_template('user_page.html')
-    variables = Context({
+    variables = RequestContext(request, {
         'username': username,
         'bookmarks': bookmarks
     })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response('user_page.html', variables)
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from bookmarks.views import *
+import os.path
+
+site_media = os.path.join(
+    os.path.dirname(__file__), 'site_media'
+)
 
 urlpatterns = patterns('',
                        # Examples:
@@ -8,7 +13,8 @@ urlpatterns = patterns('',
                        url(r'^$', main_page),
                        url(r'^user/(\w+)/$', user_page),
                        url(r'^login/$', 'django.contrib.auth.views.login'),
-
-
+                       url(r'^logout/$', logout_page),
+                       url(r'^site_media/(?P<path>.*)$','django.views.static.serve',
+                           {'document_root': site_media}),
 
                        )
